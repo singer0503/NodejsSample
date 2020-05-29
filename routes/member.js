0170795c9c6ebf8ff0bcfb6f26e67c9b77362667
黃立豪 <max@miniasp.com>
2020-05-29 16:22:33 +0800

    設定env , 資料庫連接等資料

--- routes/member.js
+++ routes/member.js
@@ -3,7 +3,34 @@ var router = express.Router();
 // 接收 post 資料
 router.post('/', function(req, res, next) {
   console.log(req.body.test)
-  res.json({'message':'ok'})
+  const memberData = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    create_date: onTime()
+  }
+  
+  res.json(memberData)
 });
 
 module.exports = router;
+
+
+
+//取得現在時間，並將格式轉成YYYY-MM-DD HH:MM:SS
+const onTime = () => {
+  const date = new Date();
+  const mm = date.getMonth() + 1;
+  const dd = date.getDate();
+  const hh = date.getHours();
+  const mi = date.getMinutes();
+  const ss = date.getSeconds();
+
+  return [date.getFullYear(), "-" +
+      (mm > 9 ? '' : '0') + mm, "-" +
+      (dd > 9 ? '' : '0') + dd, " " +
+      (hh > 9 ? '' : '0') + hh, ":" +
+      (mi > 9 ? '' : '0') + mi, ":" +
+      (ss > 9 ? '' : '0') + ss
+  ].join('');
+}
