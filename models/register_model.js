3251209167b9e76aa93dcaefe36d40938c6e1b9f
黃立豪 <max@miniasp.com>
2020-06-14 15:45:43 +0800

    實作需求-不允許相同帳號(email)重複註冊

--- models/register_model.js
+++ models/register_model.js
@@ -3,7 +3,7 @@ const db = require('./connection_db');
 module.exports = function register(memberData) {
     let result = {};
     return new Promise((resolve, reject) => {
-        db.query('insert into member_info set ?', memberData, function(err, rows){
+        db.query('select email from member_info where email = ?',memberData.email, function(err, rows){
             if(err){
                 console.log(err);
                 result.status ="註冊失敗"
@@ -11,9 +11,28 @@ module.exports = function register(memberData) {
                 reject(result);
                 return;
             }
-            result.registerMember = memberData;
-            resolve(result);
-        })
+            if(rows.length >= 1){
+                result.status = "註冊失敗";
+                result.err = "已有重複的 Email"
+                reject(result);
+            }else{
+                //無重複資料，寫入資料庫
+                db.query('insert into member_info set ?', memberData, function(err, rows){
+                    if(err){
+                        console.log(err);
+                        result.status ="註冊失敗"
+                        result.err = "伺服器錯誤，請稍候再試！"
+                        reject(result);
+                        return;
+                    }
+                    result.status ="註冊成功"
+                    result.registerMember = memberData;
+                    resolve(result);
+                })
+            }
+        });
+
+
     })
     
 }
