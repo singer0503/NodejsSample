7305700d2c0f3217c46983a9678aebe4dfaf80a5
黃立豪 <max@miniasp.com>
2020-06-14 15:56:15 +0800

    檢核 email 是否符合格式

--- controllers/modify_controller.js
+++ controllers/modify_controller.js
@@ -1,45 +1,63 @@
-const toRegister = require('../models/register_model');
+const toRegister = require("../models/register_model");
+const CheckService = require("../service/member_check");
+check = new CheckService();
 
-module.exports = class Member{
-    
-    postRegister(req, res, next){
-    
-        const memberData = {
-            name: req.body.name, 
-            email: req.body.email,
-            password: req.body.password,
-            create_date: onTime()
-        }
-        // 將資料寫入資料庫
-        toRegister(memberData).then(result => {
-            // 若寫入成功則回傳
-            res.json({
-                status: "註冊成功。",
-                result: result 
-            })
-        }, (err) => {
-            // 若寫入失敗則回傳
-            res.json({
-                result: err
-            })
-        })
+module.exports = class Member {
+  postRegister(req, res, next) {
+    const memberData = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      create_date: onTime(),
+    };
+    // 檢核 email 格式正確性
+    const checkEmail = check.checkEmail(memberData.email);
+    // 不符合email格式
+    if (checkEmail === false) {
+      res.json({
+        result: {
+          status: "註冊失敗",
+          err: "請輸入正確的 Email 格式（xxx@gmail.com）",
+        },
+      });
     }
-}
+    // 若符合email格式
+    else if (checkEmail === true) {
+      // 將資料寫入資料庫
+      toRegister(memberData).then(
+        (result) => {
+          // 若寫入成功則回傳
+          res.json({
+            status: "註冊成功。",
+            result: result,
+          });
+        },
+        (err) => {
+          // 若寫入失敗則回傳
+          res.json({
+            result: err,
+          });
+        }
+      );
+    }
+  }
+};
 
 //取得現在時間，並將格式轉成YYYY-MM-DD HH:MM:SS
 const onTime = () => {
-    const date = new Date();
-    const mm = date.getMonth() + 1;
-    const dd = date.getDate();
-    const hh = date.getHours();
-    const mi = date.getMinutes();
-    const ss = date.getSeconds();
+  const date = new Date();
+  const mm = date.getMonth() + 1;
+  const dd = date.getDate();
+  const hh = date.getHours();
+  const mi = date.getMinutes();
+  const ss = date.getSeconds();
 
-    return [date.getFullYear(), "-" +
-        (mm > 9 ? '' : '0') + mm, "-" +
-        (dd > 9 ? '' : '0') + dd, " " +
-        (hh > 9 ? '' : '0') + hh, ":" +
-        (mi > 9 ? '' : '0') + mi, ":" +
-        (ss > 9 ? '' : '0') + ss
-    ].join('');
-}
+  return [
+    date.getFullYear(),
+    "-" + (mm > 9 ? "" : "0") + mm,
+    "-" + (dd > 9 ? "" : "0") + dd,
+    " " + (hh > 9 ? "" : "0") + hh,
+    ":" + (mi > 9 ? "" : "0") + mi,
+    ":" + (ss > 9 ? "" : "0") + ss,
+  ].join("");
+};
---
+++ service/member_check.js
@@ -0,0 +1,8 @@
+module.exports = class CheckCustomer {
+    //判斷email格式
+    checkEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const result = re.test(email);
+        return result;
+    }
+}
