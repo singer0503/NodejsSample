3b087ee6712292870640f29c0d0425b2ff1e1b01
黃立豪 <max@miniasp.com>
2020-08-14 16:29:40 +0800

    新增 secret key

--- config/development_config.js
+++ config/development_config.js
@@ -6,5 +6,6 @@ module.exports = {
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE
-    }
+    },
+    sceret: process.env.MY_SECRET
 }
